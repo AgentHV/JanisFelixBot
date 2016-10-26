5badd2445f75039a56a5cc2ca5c42c0ae67d1199
Flixlix <flixmarkus1@gmail.com>
2016-10-26 20:42:17 +0200

    Update Bot.py
    Der Bot erkennt jetzt auch Befehle, wenn er mit /befehl@JanisFelixBetaBot angesprochen wird
    Code aufgeräumt

--- JanisFelixBot/Bot.py
+++ JanisFelixBot/Bot.py
@@ -1,18 +1,21 @@
+import time
 from chatter.telegramBot import TelegramBot
-#Für code muss der API-Key eingesetzt werden
+#Für code muss dein API-Key vom BotFather eingesetzt werden
 bot=TelegramBot("code")
 bot.gehe_online()
 while (1):
+    time.sleep(2)
     nachrichten = bot.hole_updates()
     for nachrichtraw in nachrichten:
         chatid=nachrichtraw.chat.id
-        userid=nachrichtraw.sender
+        userid=nachrichtraw.sender.id
         if nachrichtraw.inhalt != None:
-            nachricht=nachrichtraw.inhalt.split()
-            if (nachricht[0]=="/start" or nachricht[0] == "/start@janisfelixexperimentebot"):
+            message=nachrichtraw.inhalt.split()
+            nachricht=message[0].split("@")
+            if (nachricht[0]=="/start"):
                 bot.sende_nachricht("Hallo, ich bin der Janisfelixbot! Programmiert haben mich @flixlix und @sonixier mithilfe der vereinfachten Bot-API (https://github.com/tnstrssnr/telegram-chatter). Bitte denk daran, dass ich noch nicht 24/7 online und nur eine Beta bin!", chatid)
-            if (nachricht[0] == "/id" or nachricht[0] == "/id@janisfelixexperimentebot"):
+            if (nachricht[0] == "/id"):
                 send=("Chat-ID= "+ str(chatid) + "\n Deine ID: " + str(userid))
                 bot.sende_nachricht(send, chatid)
-            if (nachricht[0] == "/help" or nachricht[0] == "/help@janisfelixexperimentebot"):
+            if (nachricht[0] == "/help"):
                 bot.sende_nachricht("Der JanisFelixBot kann folgende Befehle: /start - Zeigt eine kurze Startnachricht \n /help - Zeigt dir diese Nachricht \n /id - Gibt dir deine ID und die des Chats", chatid)
