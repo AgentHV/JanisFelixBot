16203929624d631afbeff66737f913395374e63f
Flixlix <flixmarkus1@gmail.com>
2016-11-04 19:41:28 +0100

    Update Bot.py
    JanisFelixBot gefixt

--- JanisFelixBot/Bot.py
+++ JanisFelixBot/Bot.py
@@ -1,9 +1,10 @@
 import time
 from chatter.telegramBot import TelegramBot
-bot=TelegramBot("token einfügen")
+bot=TelegramBot("Hier den API-Key von Botfather einfügen")
 bot.gehe_online()
 otherbots = open("otherbots.txt").readlines()
 admins = open("admins.txt").readlines()
+blacklist = open("blacklist.txt")
 for x in range(0,len(admins)) :
     admins[x] = admins[x].rstrip()
     admins[x].replace("'", "")
@@ -27,33 +28,38 @@ while (1):
         chatid=nachrichtraw.chat.id
         userid=nachrichtraw.sender.id
         uservorname=nachrichtraw.sender.vorname
-        if (userid in admins):
-            admin = 1
-        else:
-            admin = 0
-        if nachrichtraw.inhalt != None:
+        nachrichtid=nachrichtraw.id
+        if (nachrichtraw.inhalt != None and str(userid) not in blacklist):
             message=nachrichtraw.inhalt.split()
             nachricht=message[0].split("@")
             if (nachricht[0]=="/start"):
-                bot.sende_nachricht("Hallo, ich bin der Janisfelixbot! Programmiert haben mich @flixlix und @sonixier mithilfe der vereinfachten Bot-API (https://github.com/tnstrssnr/telegram-chatter). Bitte denk daran, dass ich noch nicht 24/7 online und nur eine Beta bin! Link zur Repository: https://github.com/Sonixier/JanisFelixBot", chatid)
+                bot.sende_nachricht("Hallo, ich bin der Janisfelixbot! Programmiert haben mich @flixlix und @sonixier mithilfe der vereinfachten Bot-API (https://github.com/tnstrssnr/telegram-chatter). Bitte denk daran, dass ich noch nicht 24/7 online und nur eine Beta bin!", chatid, nachrichtid)
             if (nachricht[0] == "/id"):
-                send=("Chat-ID= "+ str(chatid) + "\n Deine ID: " + str(userid))
-                bot.sende_nachricht(send, chatid)
+                send=("Chat-ID= "+ str(chatid, nachrichtid) + "\n Deine ID: " + str(userid))
+                bot.sende_nachricht(send, chatid, nachrichtid)
             if (nachricht[0] == "/null"):
-                bot.sende_nachricht("​", chatid)
+                bot.sende_nachricht("​", chatid, nachrichtid)
             if (nachricht[0] == "/markdown"):
-                bot.sende_nachricht("Es gibt die folgenden Formatierungen: *bold*_italic_`fixedsys`[Link](www.google.com)", chatid)
+                bot.sende_nachricht("Es gibt die folgenden Formatierungen: *bold*_italic_`fixedsys`[Link](www.google.com)", chatid, nachrichtid)
             if (nachricht[0] == "/help"):
-                bot.sende_nachricht("Ich kann folgende Befehle: /start - Zeigt eine kurze Startnachricht \n /gruppen - Hier schlage ich dir ein paar Gruppen vor \n /gruppevorschlagen - Du hast eine eigene Gruppe oder kennst eine, die es verdient hat in meinen Vorschlägen zu erscheinen? Klick dieses Command an! \n /help - Zeigt dir diese Nachricht \n /id - Gibt dir deine ID und die des Chats", chatid)
+                bot.sende_nachricht("Ich kann folgende Befehle: /start - Zeigt eine kurze Startnachricht \n /gruppen - Hier schlage ich dir ein paar Gruppen vor \n /gruppevorschlagen - Du hast eine eigene Gruppe oder kennst eine, die es verdient hat in meinen Vorschlägen zu erscheinen? Klick dieses Command an! \n  /id - Gibt dir deine ID und die des Chats \n /help - Zeigt dir diese Nachricht \n /group - Sagt dir, in welchen Usergruppen du bist.", chatid, nachrichtid)
+            if ("ayy" in message or "ayy" in nachricht):
+                bot.sende_nachricht("lmao", chatid, nachrichtid)
+            if ("lmao" in message or "lmao" in nachricht):
+                bot.sende_nachricht("ayy", chatid, nachrichtid)
+            if ("rip" in message or "rip" in nachricht):
+                bot.sende_nachricht("rest in pieces", chatid, nachrichtid)
+            if ("janisfelixbot" in message or "JanisFelixBot" in nachricht):
+                bot.sende_nachricht("Das bin ich!", chatid, nachrichtid)
             #Die zu /gruppen gehörenden Commands
             if (nachricht[0] == "/gruppen"):
-                bot.sende_nachricht("Ich kenne die folgenden Gruppen: \n Android-Hilfe: /granh \n Spamgruppe: /grspam \n Blackjackgruppe: /grbj", chatid)
+                bot.sende_nachricht("Ich kenne die folgenden Gruppen: \n Android-Hilfe: /granh \n Spamgruppe: /grspam \n Blackjackgruppe: /grbj", chatid, nachrichtid)
             if (nachricht[0] == "/granh"):
-                bot.sende_nachricht("Die inoffizielle Gruppe von http://www.android-hilfe.de, die Gruppe in der du über Android diskutieren kannst (und jeden Morgen ein nettes ''Guten Morgen zusammen'' von Mirko kriegst)! \n Rein kommst du mit diesem Link: [Link einfügen]", chatid)
+                bot.sende_nachricht("Die inoffizielle Gruppe von http://www.android-hilfe.de, die Gruppe in der du über Android diskutieren kannst (und jeden Morgen ein nettes ''Guten Morgen zusammen'' von Mirko kriegst)! \n Rein kommst du mit diesem Link: [Link einfügen]", chatid, nachrichtid)
             if (nachricht[0] == "/grspam"):
-                bot.sende_nachricht("Hier kannst du soviel Bots, Sticker und wasauchimmer spammen wie du willst! Komm einfach rein: https://telegram.me/spamshit", chatid)
+                bot.sende_nachricht("Hier kannst du soviel Bots, Sticker und wasauchimmer spammen wie du willst! Komm einfach rein: https://telegram.me/spamshit", chatid, nachrichtid)
             if (nachricht[0] == "/grbj"):
-                bot.sende_nachricht("Eine kleine Gruppe, in der man mit dem @blackjackbot spielen kann! Kommt einfach rein: telegram.me/playblackjack", chatid)
+                bot.sende_nachricht("Eine kleine Gruppe, in der man mit dem @blackjackbot spielen kann! Kommt einfach rein: telegram.me/playblackjack", chatid, nachrichtid)
             if (nachricht[0] == "/gruppevorschlagen"):
                 if (len(message) > 1):
                     words=len(message)
@@ -62,26 +68,16 @@ while (1):
                         send = send + message[x] + " "
                     bot.sende_nachricht(send, -151573627)
                 else:
-                    bot.sende_nachricht("Bitte schicke mir diesen Command so: \n /gruppevorschlagen <Gruppenname und kurze Vorstellung der Gruppe> \n Wir werden dich dann kontaktieren. Bitte denke jedoch daran wenn du rumspammst, dass du dann einen Ban kassieren kannst!", chatid)
-            if ("ayy" in message or "ayy" in nachricht):
-                bot.sende_nachricht("lmao", chatid)
-            if ("lmao" in message or "lmao" in nachricht):
-                bot.sende_nachricht("ayy", chatid)
-            if ("rip" in message or "rip" in nachricht):
-                bot.sende_nachricht("rest in pieces", chatid)
-            if ("JanisFelixBot" in message or "JanisFelixBot" in nachricht):
-                bot.sende_nachricht("Das bin ich!", chatid)
+                    bot.sende_nachricht("Bitte schicke mir dieses Command so: \n /gruppevorschlagen <Gruppenname und kurze Vorstellung der Gruppe> \n Wir werden dich dann kontaktieren. Bitte denke jedoch daran wenn du rumspammst, dass du dann einen Ban kassieren kannst!", chatid, nachrichtid)
 # Alles was mit Usergruppen zu tun hat
             if (nachricht[0] == "/stop"):
                 if (str(userid) in admins):
-                    bot.sende_nachricht("Der Bot beendet sich jetzt!", chatid)
+                    bot.sende_nachricht("Der Bot beendet sich jetzt!", chatid, nachrichtid)
                     raise
                 else:
-                    bot.sende_nachricht("Du bist kein Admin!", chatid)
+                    bot.sende_nachricht("Du bist kein Admin!", chatid, nachrichtid)
             if (nachricht[0] == "/group"):
                 if (str(userid) in admins):
-                    bot.sende_nachricht("Du bist ein globaler Admin!", chatid)
+                    bot.sende_nachricht("Du bist ein globaler Admin!", chatid, nachrichtid)
                 else:
-                    bot.sende_nachricht("Du wurdest nicht kategorisiert!", chatid)
-
-
+                    bot.sende_nachricht("Du wurdest nicht kategorisiert!", chatid, nachrichtid)
